27d3b36765c70ee14fea676953076011b266a7fe
Saiyam Jain <ge63req@mytum.com>
2024-03-02 22:01:14 +0100

    Add tagcloud

--- src/components/extra/TagCloud.tsx
+++ src/components/extra/TagCloud.tsx
@@ -1,28 +1,46 @@
 import React, { useEffect } from "react";
-import TagCloud, { TagCloudOptions } from "TagCloud";
+import TagCloud, { TagCloudOptions } from "@frank-mayer/react-tag-cloud";
 
 export const SkillSphere = () => {
-    const container = '.tagcloud';
-    const texts = [
-        'Python', 'JS/TS',
-        'Html-CSS', 'Java', 'SAP',
-        'Firebase', 'Vercel', 'Next',
-        'C', 'C++', 'git', 'SQL', 'Spring', 'Node.js', 'R'
-    ];
-    const options: TagCloudOptions = {
-        radius: 400,
-        maxSpeed: 'fast',
-        initSpeed: 'normal',
-        keep: true,
-    };
-    useEffect(() => {
-        return () => {
-            TagCloud(container, texts, options);
-        };
-    }, []);
+    // const container = '.tagcloud';
+    // const texts = [
+    //     'Python', 'JS/TS',
+    //     'Html-CSS', 'Java', 'SAP',
+    //     'Firebase', 'Vercel', 'Next',
+    //     'C', 'C++', 'git', 'SQL', 'Spring', 'Node.js', 'R'
+    // ];
+    // const options: TagCloudOptions = {
+    //     radius: 400,
+    //     maxSpeed: 'fast',
+    //     initSpeed: 'normal',
+    //     keep: true,
+    // };
+    // useEffect(() => {
+    //     return () => {
+    //         TagCloud(container, texts, options);
+    //     };
+    // }, []);
     return (
-        <div className="flex justify-center align-center m-10">
-            <span className="tagcloud text-5xl font-bold text-slate-600 shadow-2xl drop-shadow-2xl" />
-        </div>
+        <TagCloud
+        options={(w: Window & typeof globalThis): TagCloudOptions => ({
+            radius: Math.min(500, w.innerWidth, w.innerHeight) / 2,
+            maxSpeed: "fast",
+        })}
+        onClick={(tag: string, ev: MouseEvent) => alert(tag)}
+        onClickOptions={{ passive: true }}
+    >
+        {[
+            "VSCode",
+            "TypeScript",
+            "React",
+            "Preact",
+            "Parcel",
+            "Jest",
+            "Next",
+            "ESLint",
+            "Framer Motion",
+            "Three.js",
+        ]}
+    </TagCloud>
     );
 }
